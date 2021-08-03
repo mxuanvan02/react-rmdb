21746680bfd2b4a0266503fef2673b0ca7135881
ADMIN\Administrator <mxuanvan159@gmail.com>
2021-08-03 15:34:30 +0700

    f-commit

---
+++ src/components/Home.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+// API 
+import API from '../API'
+// Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL, API_KEY } from '../config';
+// Components
+
+// Hook
+
+// Image
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+  const [state, setState] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+
+      // setState(prev => ({
+      //   ...movies,
+      //   results:
+      //     page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+      // }));
+    } catch (error) {
+      setError(true);
+    }
+  };
+  // Initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, [])
+
+  return <div>Homeee</div>
+}
+
+export default Home
+
+
+

0ba187e90c5e7fd64937902cb2a0d89db38d1df8
mxuanvan02 <mxuanvan159@gmail.com>
2021-08-21 21:28:38 +0700

    complete

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -12,6 +12,13 @@ export const Content = styled.div`
   max-width: var(--max-width);
   padding: 20px 0;
   margin: 0 auto;
+  color: var(--white);
+
+  a {
+    color: var(--white);
+    text-decoration: none;
+  }
+
 `;
 
 export const LogoImg = styled.img`
--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import API from '../../API';
 
 // Components
 import Thumb from '../Thumb';
+import Rate from '../Rate';
 
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
@@ -12,40 +14,60 @@ import NoImage from '../../images/no_image.jpg';
 // Styles
 import { Wrapper, Content, Text } from './MovieInfo.styles';
 
-const MovieInfo = ({ movie }) => (
-  <Wrapper backdrop={movie.backdrop_path}>
-    <Content>
-      <Thumb image={
-        movie.poster_path
-        ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-        : NoImage
-      }
-      clickable={false}
-      alt='movie-thumb'
-      />
-      <Text>
-        <h1>{movie.title}</h1>
-        <h3>PLOT</h3>
-        <p>{movie.overview}</p>
+// Context
+import { Context } from '../../context';
 
-        <div className='rating-directors'>
+const MovieInfo = ({ movie }) => {
+  const [user] = useContext(Context);
+
+  const handleRating = async value => {
+    const rate = await API.rateMovie(user.sessionId, movie.id, value);
+    console.log(rate);
+  }
+
+
+
+  return (
+    <Wrapper backdrop={movie.backdrop_path}>
+      <Content>
+        <Thumb image={
+          movie.poster_path
+          ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+          : NoImage
+        }
+        clickable={false}
+        alt='movie-thumb'
+        />
+        <Text>
+          <h1>{movie.title}</h1>
+          <h3>PLOT</h3>
+          <p>{movie.overview}</p>
+
+          <div className='rating-directors'>
+            <div>
+              <h3>RATING</h3>
+              <div className='score'>{movie.vote_average}</div>
+            </div>
+            <div className='director'>
+              <h3>DIRECTOR{movie.directors.length > 1 ? "S" : ""}</h3>
+              {movie.directors.map(director => (
+                <p key={director.credit_id}>{director.name}</p>
+              ))}
+            </div>
+          </div>
+          {user && (
           <div>
-            <h3>RATING</h3>
-            <div className='score'>{movie.vote_average}</div>
-          </div>
-          <div className='director'>
-            <h3>DIRECTOR{movie.directors.length > 1 ? "S" : ""}</h3>
-            {movie.directors.map(director => (
-              <p key={director.credit_id}>{director.name}</p>
-            ))}
-          </div>
-        </div>
-      </Text>
+            <p>Rate Movie</p>
+            <Rate callback={handleRating} />
+          </div> 
+          )}
+        </Text>
 
-    </Content>
-  </Wrapper>
+      </Content>
+    </Wrapper>
 
-);
+  );
+}
 
 export default MovieInfo 
 
